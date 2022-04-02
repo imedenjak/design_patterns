d949f2fb97d35ee4ea8a053912b11bda075198c4
imedenjak <ivan.medenjak@gmail.com>
2022-04-02 10:24:21 +0200

    Singleton

---
+++ Singleton/Implementation.cs
@@ -0,0 +1,14 @@
+namespace Singleton;
+
+public class Logger
+{
+    private static readonly Lazy<Logger> LazyLogger = new(() => new Logger());
+    public static Logger Instance => LazyLogger.Value;
+    protected Logger()
+    {
+    }
+    public void Log(string message)
+    {
+        Console.WriteLine($"Message to log: {message}");
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Singleton;
+
+Console.WriteLine("Hello, World!");
+
+var logger1 = Logger.Instance;
+var logger2 = Logger.Instance;
+
+if (logger1 == logger2 && logger2 == Logger.Instance)
+{
+    Console.WriteLine("Instances are the same.");
+}
+
+logger1.Log($"Message from {nameof(logger1)}");
+logger2.Log($"Message form {nameof(logger2)}");
+
+Console.ReadKey();
