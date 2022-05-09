01e21b12cf4c0e1fdcaa8bf1d499b9ab2d6e8bf4
imedenjak <ivan.medenjak@gmail.com>
2022-05-09 13:07:00 +0200

    Object adapter implementation

---
+++ Adapter/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Adapter;
+
+Console.WriteLine("Adapter");
+
+ICityAdapter adapter = new CityAdapter();
+var city = adapter.GetCity();
+
+Console.WriteLine($"{city.FullName}, {city.Inhabitants}");
+Console.ReadLine();
